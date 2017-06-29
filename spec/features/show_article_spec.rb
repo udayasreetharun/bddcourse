97b8e3fb73f87f19370da39404ad84b43eec37ed
Udaya sree <Udayasreetharun@gmail.com>
2017-06-29 15:17:50 +0530

    hiding the delete and edit method for non user

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -17,10 +17,21 @@ expect(page).not_to have_link("Edit article")
 expect(page).not_to have_link("Delete article")
 end
 scenario "A user signed in then lists all articles" do
+	login_as (@fred)
 visit "/"
-login_as (@fred)
+
 click_link @article.title
 expect(page).to have_link("Edit article")
 expect(page).to have_link("Delete article")
+end
+scenario "A signed in owner sees both links" do 
+login_as(@john)
+visit "/"
+click_link @article.title
+expect(page).to have_content(@article.title) 
+expect(page).to have_content(@article.body) 
+expect(current_path).to eq(article_path(@article))
+expect(page).to have_link("Edit Article")
+expect(page).to have_link("Delete Article") 
 end
 end
