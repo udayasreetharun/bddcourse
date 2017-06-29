2940641dcf37a10ccab5472dc696908948df8d8f
Udaya sree <Udayasreetharun@gmail.com>
2017-06-29 14:39:09 +0530

    Show the article for logged in user

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 RSpec.feature "Listing Articles" do 
 before do
-	john = User.create(email: "john@example.com", password: "password")
+	@john = User.create(email: "john@example.com", password: "password")
 
-	@article1 = Article.create(title:"the first article",body:"description about first article",user: john)
-	@article2 =Article.create(title:"the second article",body:"description about second article",user: john)
+	@article1 = Article.create(title:"the first article",body:"description about first article",user: @john)
+	@article2 =Article.create(title:"the second article",body:"description about second article",user: @john)
 end
 	scenario "A user lists all article" do
 	visit "/"
@@ -31,5 +31,28 @@ expect(page).not_to have_link(@article2.title)
 within ("h1#no-articles") do
 expect(page).to have_content("No Articles Created")
 end 
+end
+scenario "with articles created and user not signed in" do
+	visit "/"
+	expect(page).to have_content(@article1.title) 
+expect(page).to have_content(@article1.body) 
+expect(page).to have_content(@article2.title) 
+expect(page).to have_content(@article2.body) 
+expect(page).to have_link(@article1.title) 
+expect(page).to have_link(@article2.title)
+expect(page).not_to have_link("New Article")
+
+
+	end
+	scenario "with articles created and user signed in" do 
+login_as(@john)
+visit "/"
+expect(page).to have_content(@article1.title) 
+expect(page).to have_content(@article1.body) 
+expect(page).to have_content(@article2.title) 
+expect(page).to have_content(@article2.body) 
+expect(page).to have_link(@article1.title) 
+expect(page).to have_link(@article2.title) 
+expect(page).to have_link("New Article")
 end
 end
--- spec/features/user_signing_in_article_spec.rb
+++ spec/features/user_signing_in_article_spec.rb
@@ -1,17 +1,18 @@
 require "rails_helper"
-RSpec.feature "Users signin" do 
+RSpec.feature "Signing out signed-in users" do 
 before do
-@john = User.create!(email: "john@example.com", password: "password") 
-end
-scenario "with valid credentials" do
-visit "/"
+@john = User.create!(email: "john@example.com", password: "password")
+visit '/'
 click_link "Sign in"
 fill_in "Email", with: @john.email 
 fill_in "Password", with: @john.password 
 click_button "Log in"
-expect(page).to have_content("Signed in successfully.") 
-expect(page).to have_content("Signed in as #{@john.email}") 
-expect(page).not_to have_link("Sign in") 
-expect(page).not_to have_link("Sign up")
-end 
+end
+
+scenario do
+visit "/"
+click_link "Sign out"
+expect(page).to have_content("Signed out successfully.")
+expect(page).not_to have_content("Sign out") 
+end
 end
